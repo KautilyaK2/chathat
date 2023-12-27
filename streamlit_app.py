26378603fcddb21276ce9a7f6984266258d55689
KautilyaK2 <127334503+KautilyaK2@users.noreply.github.com>
2023-12-27 12:23:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
 import streamlit as st
-prompt = st.chat_input("Say something")
-if prompt:
-    st.markdown(f"User has sent the following prompt: {prompt}")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+# React to user input
+if prompt := st.chat_input("What's up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
