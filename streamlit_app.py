143226808817e63fd8e74982b9f33b033e527789
KautilyaK2 <127334503+KautilyaK2@users.noreply.github.com>
2023-12-27 12:39:37 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
+from transformers import AutoTokenizer
+from petals import AutoDistributedModelForCausalLM
 import streamlit as st
-import petals as pt
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# Choose any model available at https://health.petals.dev
+model_name = "petals-team/StableBeluga2"  # This one is fine-tuned Llama 2 (70B)
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-# React to user input
-if prompt := st.chat_input("What's up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# Connect to a distributed network hosting model layers
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoDistributedModelForCausalLM.from_pretrained(model_name)
+system_prompt = "### System:\nYou are Stable Beluga, an AI that is very precise. Be as accurate as you can.\n\n"
+conv=system_prompt
+message = st.chat_input('Message')
+if message:
+    prompt = f"### User: {message}\n\n### Assistant:\n"
+    conv+=prompt
+    # Run the model as if it were on your computer
+    inputs = tokenizer(conv, return_tensors="pt")["input_ids"]
+    outputs = model.generate(inputs, max_new_tokens=256)
+    with st.chat_message('user'):
+        st.write(tokenizer.decode(outputs[0])[3:-4])
